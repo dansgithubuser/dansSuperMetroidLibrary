b9756b41a3f2678df4d777af7f0c22a1f94cebc4
Dan <dansonlinepresence@gmail.com>
2018-07-02 20:26:00 -0400

    failing test for bug 3 from https://github.com/dansgithubuser/dansSuperMetroidLibrary/issues/1

--- test.cpp
+++ test.cpp
@@ -8,19 +8,33 @@ using namespace sm;
 unsigned decompress(const Buffer& source, uint32_t offset, Buffer* destination);
 void compress(const Buffer& source, Buffer& destination);
 
+//=====helpers=====//
+#define COMPDECOMP(UNCOMPRESSED){\
+	Buffer compressed, decompressed;\
+	compress(UNCOMPRESSED, compressed);\
+	decompress(compressed, 0, &decompressed);\
+	EXPECT(decompressed.size(), UNCOMPRESSED.size())\
+	for(size_t i=0; i<decompressed.size(); ++i) EXPECT(decompressed[i], UNCOMPRESSED[i])\
+}
+
+void pushIncompressible(Buffer& b, unsigned i, uint8_t mask=0){
+	b.push_back((    i%128)^mask);
+	b.push_back((128+i/128)^mask);
+}
+
 //=====main=====//
 int main(){
 	{
 		Buffer incompressible;
-		for(unsigned i=0; i<1025; i+=2){
-			incompressible.push_back(i%128);
-			incompressible.push_back(128+i/128);
-		}
-		Buffer compressed, decompressed;
-		compress(incompressible, compressed);
-		decompress(compressed, 0, &decompressed);
-		EXPECT(decompressed.size(), incompressible.size())
-		for(size_t i=0; i<decompressed.size(); ++i) EXPECT(decompressed[i], incompressible[i])
+		for(unsigned i=0; i<1025; i+=2) pushIncompressible(incompressible, i);
+		COMPDECOMP(incompressible);
+	}
+	{
+		Buffer uncompressed;
+		for(unsigned i=0; i<0x10001; ++i) uncompressed.push_back(0);
+		for(unsigned i=0; i<4; i+=2) pushIncompressible(uncompressed, i);
+		for(unsigned i=0; i<1024; ++i) uncompressed.push_back(uncompressed[0x10001+i%8]^0xFFu);
+		COMPDECOMP(uncompressed);
 	}
 	return 0;
 }
